6fceb8e5616a0a374f1541d4bd292cba0a498224
Arnau Canela <arnau.canela22@gmail.com>
2024-05-17 23:11:22 +0200

    new process commit 3

--- src/app/components/Process.js
+++ src/app/components/Process.js
@@ -1,14 +1,12 @@
-"use client";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { motion } from "framer-motion";
 import Image from "next/image";
-import Link from "next/link";
 import Translator from "./Translator";
 import { poppins } from "./MainScreen";
 import ProcessAnimation1 from "../assets/ProcessImage1.png";
-import ProcessPoints from "../assets/ProcessPoints.png";
 import { be_vietnam_pro } from "../page";
-import { useInView, InView } from "react-intersection-observer";
+import { useInView } from 'react-intersection-observer';
+import { useScroll } from 'react-use';
 
 const processSteps = [
   {
@@ -19,118 +17,90 @@ const processSteps = [
     image: ProcessAnimation1,
   },
   {
-    number: "01",
-    title: "Get In Touch",
-    description:
-      "The first step in the process is to get in touch. For that, we offer both our contact form (to explain the idea you have in mind or ask any question) and our project planner (if you have a clearer idea of your project). We let's responde you the faster we can, explaining all the details (including the budget).",
+    number: "02",
+    title: "Step Two",
+    description: "If you agree with the budget, estimated time, and all the other details, we will can start working on your project. Most times you will have two persons (one designer and one developer) working on your project. During the process stay in touch to ensure that we create the project you had in your mind.",
     image: ProcessAnimation1,
   },
   {
-    number: "01",
-    title: "Get In Touch",
-    description:
-      "The first step in the process is to get in touch. For that, we offer both our contact form (to explain the idea you have in mind or ask any question) and our project planner (if you have a clearer idea of your project). We let's responde you the faster we can, explaining all the details (including the budget).",
+    number: "03",
+    title: "Step Three",
+    description: "Finally, it will be time to deliver the final product. If you like the product, we will consider the project completed. On the other hand, if you're not satisfied, we can make the necessary adjustments (at no extra cost), or cancel the contract if the delivered product is significantly different from the one initially requested (in this case, no fees will be charged for the services provided).",
     image: ProcessAnimation1,
   },
   {
-    number: "01",
-    title: "Get In Touch",
-    description:
-      "The first step in the process is to get in touch. For that, we offer both our contact form (to explain the idea you have in mind or ask any question) and our project planner (if you have a clearer idea of your project). We let's responde you the faster we can, explaining all the details (including the budget).",
+    number: "04",
+    title: "Step Four",
+    description: "Even though we have declared the project completed, this does not mean that the contract between Skaili Agency (as a web service provider) and you has ended. We will therefore handle both the maintenance of your website and the correction of any minor errors that may arise in the future.",
     image: ProcessAnimation1,
   },
 ];
 
+const Step = ({ step, index }) => {
+  return (
+    <div key={index} className="process__main__target__div">
+      <div className="step__marker" />
+      <div className="process__step__div">
+        <div className="process__step__text__div">
+          <h3 className="process__step__title">
+            {step.title} <span className="text-[#3B71FE]">.</span>
+          </h3>
+          <span className="process__step__description">
+            {step.description}
+          </span>
+        </div>
+        <Image src={step.image} alt="illustration" />
+      </div>
+    </div>
+  );
+};
+
 function Process() {
-  const [isSticky, setIsSticky] = useState(false);
-  const [view1, setView1] = useState(false);
-  const [view2, setView2] = useState(false);
-  const [view3, setView3] = useState(false);
-  const [view4, setView4] = useState(false);
-  const { ref, inView } = useInView({ threshold: 0.5 });
+  const [currentStep, setCurrentStep] = useState(processSteps[0].number);
+  const containerRef = useRef(null);
+  const { y } = useScroll(containerRef);
+  const stepRefs = useRef([]);
+  const { height } = useInView({ ref: containerRef });
 
   useEffect(() => {
-    setIsSticky(inView);
-  }, [inView]);
+    if (!height) return;
+
+    const stepHeight = height / processSteps.length;
+    const currentStepNumber = Math.ceil(y / stepHeight);
+    setCurrentStep(processSteps[currentStepNumber - 1].number);
+  }, [y, height]);
 
   return (
     <Translator>
       {(translations) => (
-        <motion.section className={`process__section ${isSticky ? "sticky" : ""}`} id="process__section">
-          <motion.div
-            className={`process__sticky__content`}
-            ref={ref}
-          >
-            <span className="process__section__name">PROCESS</span>
-            <div className="process__title__div">
-              <h2 className={`process__main__text ${poppins.className}`}>
-                KNOW ABOUT OUR
-                <span className="text-[#586CF5]"> WORKING PROCESS</span>
-              </h2>
+        <div ref={containerRef} className="relative ">
+          <motion.section className="process__section" id="process__section">
+            <motion.div className="process__sticky__content sticky">
+              <span className="process__section__name">PROCESS</span>
+              <div className="process__title__div">
+                <h2 className={`process__main__text ${poppins.className}`}>
+                  KNOW ABOUT OUR
+                  <span className="text-[#586CF5]"> WORKING PROCESS</span>
+                </h2>
+              </div>
+            </motion.div>
+            <div className="process__main__div">
+              <span className={`${be_vietnam_pro.className} step__number`}>
+                {currentStep}
+              </span>
+              <div className="process__line__div" />
+              <div className="process__steps__col__div">
+                {processSteps.map((step, index) => (
+                  <Step
+                    key={index}
+                    step={step}
+                    index={index}
+                  />
+                ))}
+              </div>
             </div>
-            
-          </motion.div>
-          <div className="process__main__div">
-            <span className={`${be_vietnam_pro.className} step__number`}>
-              01
-            </span>
-            <div className="process__line__div" />
-            <div className="process__steps__col__div">
-              {processSteps.map((step, index) => (
-                <InView
-                  as="div"
-                  onChange={(inView, entry) => {
-                    if (index == 0) {
-                      setView1(inView);
-                    }
-                    if (index == 1) {
-                      setView2(inView);
-                    }
-                    if (index == 2) {
-                      setView3(inView);
-                    }
-                    if (index == 3) {
-                      setView4(inView);
-                    }
-                  }}
-                  key={index}
-                  className={`process__main__target__div ${
-                    index == 0
-                      ? view1
-                        ? "in-view"
-                        : ""
-                      : index == 1
-                      ? view2
-                        ? "in-view"
-                        : ""
-                      : index == 2
-                      ? view3
-                        ? "in-view"
-                        : ""
-                      : index == 3
-                      ? view4
-                        ? "in-view"
-                        : ""
-                      : ""
-                  }`}
-                >
-                  <div className="step__marker" />
-                  <div className="process__step__div">
-                    <div className="process__step__text__div">
-                      <h3 className="process__step__title">
-                        {step.title} <span className="text-[#3B71FE]">.</span>
-                      </h3>
-                      <span className="process__step__description">
-                        {step.description}
-                      </span>
-                    </div>
-                    <Image src={step.image} alt="illustration" />
-                  </div>
-                </InView>
-              ))}
-            </div>
-          </div>
-        </motion.section>
+          </motion.section>
+        </div>
       )}
     </Translator>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -24,6 +24,10 @@ import { useEffect, useRef, useState } from "react";
 import Translator from "./components/Translator";
 import { useScroll } from "react-use";
 import { motion } from "framer-motion";
+import ProcessAnimation1 from "./assets/ProcessImage1.png";
+import ProcessAnimation2 from "./assets/ProcessImage2.png";
+import ProcessAnimation3 from "./assets/ProcessImage3.png";
+import ProcessAnimation4 from "./assets/ProcessImage4.png";
 
 import {
   Shanti,
@@ -107,24 +111,51 @@ const sendMessage = async (values) => {
   }
 };
 
-const sidebar = {
-  open: (height = 1000) => ({
-    clipPath: `circle(${height * 2 + 200}px at 40px 40px)`,
-    transition: {
-      type: "spring",
-      stiffness: 20,
-      restDelta: 2,
-    },
-  }),
-  closed: {
-    clipPath: "circle(30px at 40px 40px)",
-    transition: {
-      delay: 0.5,
-      type: "spring",
-      stiffness: 400,
-      damping: 40,
-    },
+const processSteps = [
+  {
+    number: "01",
+    title: "Get In Touch",
+    description:
+      "The first step in the process is to get in touch. For that, we offer both our contact form (to explain the idea you have in mind or ask any question) and our project planner (if you have a clearer idea of your project). We let's responde you the faster we can, explaining all the details (including the budget).",
+    image: ProcessAnimation1,
   },
+  {
+    number: "02",
+    title: "Start Working",
+    description: "If you agree with the budget, estimated time, and all the other details, we will can start working on your project. Most times you will have two persons (one designer and one developer) working on your project. During the process stay in touch to ensure that we create the project you had in your mind.",
+    image: ProcessAnimation2,
+  },
+  {
+    number: "03",
+    title: "Work Delivering",
+    description: "Finally, it will be time to deliver the final product. If you like the product, we will consider the project completed. On the other hand, if you're not satisfied, we can make the necessary adjustments (at no extra cost), or cancel the contract if the delivered product is significantly different from the one initially requested (in this case, no fees will be charged for the services provided).",
+    image: ProcessAnimation3,
+  },
+  {
+    number: "04",
+    title: "Maintenance",
+    description: "Even though we have declared the project completed, this does not mean that the contract between Skaili Agency (as a web service provider) and you has ended. We will therefore handle both the maintenance of your website and the correction of any minor errors that may arise in the future.",
+    image: ProcessAnimation4,
+  },
+];
+
+const Step = ({ step, index }) => {
+  return (
+    <div key={index} className="process__main__target__div">
+      <div className="step__marker" />
+      <div className="process__step__div">
+        <div className="process__step__text__div">
+          <h3 className="process__step__title">
+            {step.title} <span className="text-[#3B71FE]">.</span>
+          </h3>
+          <span className="process__step__description">
+            {step.description}
+          </span>
+        </div>
+        <Image src={step.image} alt="illustration" />
+      </div>
+    </div>
+  );
 };
 
 export default function Home() {
@@ -135,6 +166,24 @@ export default function Home() {
 
   const scrollRef = useRef(null);
   const { x, y } = useScroll(scrollRef);
+
+  const [currentStep, setCurrentStep] = useState(processSteps[0].number);
+  const stepRefs = useRef([]);
+  const containerRef = useRef(null)
+
+  useEffect(() => {
+    if (y <= 4000) {
+      setCurrentStep(processSteps[0].number)
+    }
+    if (y > 4000 && y <= 4300) {
+      setCurrentStep(processSteps[1].number)
+    } else if (y > 4300 && y <= 4600) {
+      setCurrentStep(processSteps[2].number)
+    }
+    else if (y > 4700 && y <= 5900) {
+      setCurrentStep(processSteps[3].number)
+    }
+  }, [y]);
 
   useEffect(() => {
     if (y >= 2) {
@@ -865,7 +914,34 @@ export default function Home() {
       <TechWrapper images={images} />
       <About />
       <Services />
-      <Process />
+      <div ref={containerRef}>
+          <motion.section className="process__section" id="process__section">
+            <motion.div className="process__sticky__content sticky">
+              <span className="process__section__name">PROCESS</span>
+              <div className="process__title__div">
+                <h2 className={`process__main__text ${poppins.className}`}>
+                  KNOW ABOUT OUR
+                  <span className="text-[#586CF5]"> WORKING PROCESS</span>
+                </h2>
+              </div>
+            </motion.div>
+            <div className="process__main__div">
+              <span className={`${be_vietnam_pro.className} step__number`}>
+                {currentStep}
+              </span>
+              <div className="process__line__div" />
+              <div className="process__steps__col__div">
+                {processSteps.map((step, index) => (
+                  <Step
+                    key={index}
+                    step={step}
+                    index={index}
+                  />
+                ))}
+              </div>
+            </div>
+          </motion.section>
+        </div>
       <Questions />
       <BottomCarousel />
       <Translator>
